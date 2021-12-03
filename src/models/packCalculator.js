2bb9d8b97ab2857dc95407722e62f2f71c43a243
tomlowen <tom.r.lowen@googlemail.com>
2021-12-03 17:25:27 +0000

    refacotred alogrithm

--- src/models/packCalculator.js
+++ src/models/packCalculator.js
@@ -1,14 +1,13 @@
 const packCalculator = (packSizes, order) => {
   //takes in an array of pack sizes e.g. [5000, 2000, 250, 100] and an order volume e.g 3500
-  const p = packSizes.sort((a, b) => a - b);
+  const packs = packSizes.sort((a, b) => a - b);
   let fulfillment = {};
-  let i = p.length - 1;
+  let i = packs.length - 1;
 
   while (order > 0) {
-    let breakpoint = Math.max(p[i] - p[0], p[i - 1]);
-    if (order > breakpoint || i === 0) {
-      fulfillment[p[i]] = fulfillment[p[i]] + 1 || 1;
-      order -= p[i];
+    if (order > packs[i] - packs[0] || i === 0) {
+      fulfillment[packs[i]] = fulfillment[packs[i]] + 1 || 1;
+      order -= packs[i];
     } else {
       i--;
     }
@@ -16,6 +15,5 @@ const packCalculator = (packSizes, order) => {
   console.log(fulfillment);
   return fulfillment;
 };
-
 
 export default packCalculator;

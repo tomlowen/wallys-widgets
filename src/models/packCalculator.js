af97dd93825264570b32f2142d843c888a1c27a6
tomlowen <tom.r.lowen@googlemail.com>
2021-12-03 16:52:44 +0000

    edge case for calc handled

--- src/models/packCalculator.js
+++ src/models/packCalculator.js
@@ -1,19 +1,21 @@
 const packCalculator = (packSizes, order) => {
   //takes in an array of pack sizes e.g. [5000, 2000, 250, 100] and an order volume e.g 3500
-  const p = packSizes.sort((a, b) => a - b).reverse();
+  const p = packSizes.sort((a, b) => a - b);
   let fulfillment = {};
-  let i = 0;
+  let i = p.length - 1;
 
   while (order > 0) {
-    if (order > p[i] - p[p.length - 1] || i === p.length - 1) {
+    let breakpoint = Math.max(p[i] - p[0], p[i - 1]);
+    if (order > breakpoint || i === 0) {
       fulfillment[p[i]] = fulfillment[p[i]] + 1 || 1;
       order -= p[i];
     } else {
-      i++;
+      i--;
     }
   }
   console.log(fulfillment);
   return fulfillment;
 };
+
 
 export default packCalculator;
